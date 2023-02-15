9a992f20f85e5c2044ed204349cfcde705753793
Temps233 <temps233@outlook.com>
2023-02-15 16:03:52 +0800

    Update API

--- src/vispreviewer/vispreviewer.py
+++ src/vispreviewer/vispreviewer.py
@@ -1,6 +1,18 @@
-SUCCESS = (True, None)
+class VisOpResult:
+    def __init__(self, success, err_msg):
+        self.success = success
+        self.err_msg = err_msg
+    
+    def __iter__(self):
+        return iter([self.success, self.err_msg])
 
-class VisPreviwer:
+def VisOpSuccess():
+    return VisOpResult(True, None)
+
+def VisOpFailure(err_msg):
+    return VisOpResult(False, err_msg)
+
+class VisPreviewer:
     def __init__(
         self,
         code,
@@ -9,18 +21,20 @@ class VisPreviwer:
         self.curr_co_index = -1
         self.curr_code = None
         success, err_msg = self.step()
+        if not success:
+            err_msg
     
     def step(self):
         self._step()
         if self.curr_code is None:
-            return False, "it's the end of the code!"
-        return SUCCESS
+            return VisOpFailure("it's the end of the code!")
+        return VisOpSuccess()
     
     def jump(self, lineno):
         self._jump(lineno)
         if self.curr_code is None:
-            return False, "the code just have {} lines!"
-        return SUCCESS
+            return VisOpFailure("the code just have {} lines!")
+        return VisOpSuccess()
 
     def _step(self):
         self.curr_co_index += 1
